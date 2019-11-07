ec97b6541f28f1b77af2d1e84d19bb1853bc5454
Diego <dieggo554@gmail.com>
2019-11-07 17:11:30 +0100

    -Añadido enum Habitat.java

---
+++ Herencia_Animales/src/herencia_animales/Habitat.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia_animales;
+
+/**
+ *
+ * @author pc
+ */
+public enum Habitat {
+    NO_DEFINIDO, ACUATICO, TERRESTRE, ANFIBIO
+}
