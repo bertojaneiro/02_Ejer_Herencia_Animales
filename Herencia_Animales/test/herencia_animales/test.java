3ede4b170c53eb6d6aa7e8887e1371391320f1a6
Diego <dieggo554@gmail.com>
2019-11-08 09:37:16 +0100

    -Test perro, percebe, herencia y polimorfismo

--- Herencia_Animales/test/herencia_animales/test.java
+++ Herencia_Animales/test/herencia_animales/test.java
@@ -16,21 +16,80 @@ import static org.junit.Assert.*;
  */
 public class test {
 
-     @Test
-     public void testHerenciaAnimales() {
-         Perro perro = new Perro();
-         
-         perro.setEdad(2);
-         assertEquals(perro.getEdad(), 2);
-         
-         perro.setPeso(10f);
-         assertEquals(perro.getPeso(), 10f, 0);
-         
-         perro.setRaza("Pastor alemán");
-         assertEquals(perro.getRaza(), "Pastor alemán");
-         
-         perro.setNombre("Pluto");
-         assertEquals(perro.getNombre(), "Pluto");
-     }
-     
+    @Test
+    public void testHerenciaAnimalesPerro() {
+        Perro perro = new Perro();
+
+        perro.setEdad(2);
+        assertEquals(perro.getEdad(), 2);
+
+        perro.setPeso(10f);
+        assertEquals(perro.getPeso(), 10f, 0);
+
+        perro.setRaza("Pastor alemán");
+        assertEquals(perro.getRaza(), "Pastor alemán");
+
+        perro.setNombre("Pluto");
+        assertEquals(perro.getNombre(), "Pluto");
+
+        assertEquals(perro.getHabitat(), Habitat.TERRESTRE);
+
+        perro.alimentar("hueso");
+
+        perro.desplazar();
+
+        System.out.println(perro);
+
+        Animal.mostrar(perro);
+    }
+    
+    @Test
+    public void testHerenciaAnimalesPercebe() {
+        Percebe percebe = new Percebe();
+
+        percebe.setEdad(1);
+        assertEquals(percebe.getEdad(), 1);
+
+        percebe.setPeso(0.1f);
+        assertEquals(percebe.getPeso(), 0.1f, 0);
+
+        percebe.setNombre("Toby");
+        assertEquals(percebe.getNombre(), "Toby");
+
+        assertEquals(percebe.getHabitat(), Habitat.ACUATICO);
+
+        percebe.alimentar("fitoplacton");
+
+        percebe.desplazar();
+
+        System.out.println(percebe);
+
+        Animal.mostrar(percebe);
+    }
+
+    @Test
+    public void testHerenciaAnimales() {
+        Perro pluto = new Perro("Pluto", 2);
+        Perro pancho = new Perro("Pancho", 3);
+        Percebe toby = new Percebe("Toby", 1);
+        Percebe juan = new Percebe("Juan", 0);
+        
+        Animal[] animales = {pluto, pancho, toby, juan};
+        
+        System.out.println("---------");
+        
+        for (Animal animal : animales) {
+            
+            animal.alimentar("alimento");
+
+            animal.desplazar();
+
+            System.out.println(animal);
+
+            Animal.mostrar(animal);
+            
+            System.out.println();
+        }
+        System.out.println("---------");
+    }
 }
